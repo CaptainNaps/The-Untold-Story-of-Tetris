cc0cd2fe88ec5f0fefcd61b4e093b455e5f22def
jadeloupret <cryomatik@gmail.com>
2016-11-26 01:21:49 -0800

    Lel j'suis toujours autant une plaie avec Git, should be fine now

---
+++ TP3Etu/TP3/Options.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace TP3
+{
+  partial class Options
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // Options
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ControlText;
+      this.ClientSize = new System.Drawing.Size(1480, 1223);
+      this.ForeColor = System.Drawing.Color.Lime;
+      this.Name = "Options";
+      this.Text = "Options de jeu";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
